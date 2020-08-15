aaba14ee2c67532d38e45968bc0ee6579a290eef
Randal Vance Cunanan <randal@randalvance.net>
2020-08-15 20:49:25 +0800

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import helloWorld from './helloWorld';
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    helloWorld();
+    res.send('Express + Typescript Server');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}...`);
+});
